c5fa77b401b23ea37f99716ba05e1ab0645a44ed
Suman Kumar <sktechno7102003@gmail.com>
2026-08-14 20:09:13 +0530

    updated example
    structured output from groq model using langchain and pydantic

--- examples/1.2_getting_structured_output_from_llm.py
+++ examples/1.2_getting_structured_output_from_llm.py
@@ -1,15 +1,25 @@
-# PS -> 
+# PS -> Call llm, but instead of getting chat output, output must be in JSON format
 
 import os # isse hum python ko apne system se baat krne ka access dete hai, like directory load krne ka
 from dotenv import load_dotenv # ya bina API KEy hardcode kiye usko import krta hai apne code me
 from langchain_groq import ChatGroq # apna model iske andar hai
+from pydantic import BaseModel, Field
 
-load_dotenv() # .env file me jo v api key humne likha hai ye usko load kr rha hai code. me
+load_dotenv() # .env file me jo v api key humne likha hai ye usko load kr rha hai code me
+
+# Defining the exact structure of output we want using pydantic
+class CompetitorData(BaseModel):
+    company_name: str = Field(description="Name of the Competitor")
+    has_free_tier : bool = Field(description = "True if they offer a free tier, else false")
+    core_features : list[str] = Field(description="List of maximum 3 features mentioned")
+
 
 # importing the model
 model = ChatGroq(
     model="llama-3.3-70b-versatile",
     temperature=0)
+structured_model = model.with_structured_output(CompetitorData)
+
 
 # sytem me hm model ko initial set of instruction dete hai before providing user input
 # isme hum usko uska identity, role, behviours wagare batate hai
@@ -18,7 +28,7 @@ messages = [
         "system",
         "You are a helpful Market Research Analysis Agent. Give competitor Analysis of the company the user tells you.",
     ),
-    ("human", "Give me Competitor analysis of Microsoft"), # isme hum actual user input de rhe hai
+    ("human", "Give me Competitor analysis of Chess.com")
 ]
-ai_msg = model.invoke(messages) # llm ko invoke krke aaye reply ko store kiya
-print(ai_msg.content) # .content ko print krne se sirf answer wala part hume dikhega
+ai_msg = structured_model.invoke(messages) # llm ko invoke krke aaye reply ko store kiya
+print(ai_msg) #content ko print krne se sirf answer wala part hume dikhega
